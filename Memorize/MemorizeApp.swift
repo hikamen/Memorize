69658b3d75b63a56b1684d1f1b22eb904c381142
chenkaimin <chenkaimin@rfchina.com>
2022-03-08 15:46:36 +0800

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Kamen on 2022/3/8.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
